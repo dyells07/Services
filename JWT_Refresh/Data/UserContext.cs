d640b64f70b706b182d0d218f3659af634949864
dyells07 <bhimkhanal999@gmail.com>
2025-02-13 22:53:57 +0545

    struct : JWT refresh

---
+++ JWT_Refresh/Data/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using JWT_Refresh.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace JWT_Refresh.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+        public DbSet<LoginModel>? LoginModels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LoginModel>().HasData(new LoginModel
+            {
+                Id = 1,
+                UserName = "johndoe",
+                Password = "def@123"
+            });
+        }
+    }
+}
---
+++ JWT_Refresh/Models/AuthenticatedResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Refresh.Models
+{
+    public class AuthenticatedResponse
+    {
+        public string? Token { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
